6d5bbfd50e6df86c25f7d176ccad3206b81cc169
zverseckas <zverseckas@gmail.com>
2017-12-18 16:56:42 +0200

    Tabs cleanup

--- som.py
+++ som.py
@@ -7,16 +7,16 @@ class SOM(object):
 
   @staticmethod
   def rand_map(shape, input_size):
-  	return [
-  		[Neuron(i, j, input_size) for i in range(shape[0])]
-  		for j in range(shape[1])
-  	]
+    return [
+      [Neuron(i, j, input_size) for i in range(shape[0])]
+      for j in range(shape[1])
+    ]
  
   def shape(self):
     return self._shape
 
   def map(self):
-  	return self._map
+    return self._map
 
 
 som = SOM(shape=(5,5), input_size=4)
